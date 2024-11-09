0b688f48acebe082a3b5324b3e1c0dea2bc741e9
jshishimaru <jbsss785@gmail.com>
2024-11-09 14:48:45 +0530

    add instruction struct

--- src/machine.hpp
+++ src/machine.hpp
@@ -2,9 +2,15 @@
 #define MACHINE_HPP
 
 #include <vector>
+#include <string>
 #include "alu.hpp"
 #include "io.hpp"
 using namespace std;
+
+struct Instruction{
+	string name;
+	int operand;
+};
 
 class Machine{
 
@@ -24,8 +30,6 @@ class Machine{
 		this->returnStack = vector<int>(1024,0);
 		this->pc = 0;
 	}
-
-	
 
 
 
