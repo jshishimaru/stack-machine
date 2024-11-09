e4bd58733d37c71499f7aea9a6e657e9cd15a44e
jshishimaru <jbsss785@gmail.com>
2024-11-09 17:00:40 +0530

    fix alu pointers

--- src/alu.hpp
+++ src/alu.hpp
@@ -10,187 +10,187 @@ class ALU{
 
 	public:
 
-	vector <int> dataStack;
-	vector <int> returnStack;
-	int pc;
+	vector <int>* dataStack;
+	vector <int>* returnStack;
+	int* pc;
 	bool gt;
 	bool eq;
 
-	void setStacks( vector <int> &dataStack, vector <int> &returnStack, int &pc ){
-		this->dataStack = dataStack;
-		this->returnStack = returnStack;
-		this->pc = pc;
+	void setStacks( vector <int> &dataStack , vector <int> &returnStack, int &pc ){
+		this->dataStack = &dataStack;
+		this->returnStack = &returnStack;
+		this->pc = &pc;
 		gt=0;
 		eq=0;
 	}
 
 	int push( int operand ){
-		dataStack.push_back(operand);
+		dataStack->push_back(operand);
 		return 0;	
 	}
 
 	int pop(){
-		if (dataStack.size() == 0) {
+		if (dataStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		dataStack.pop_back();
+		dataStack->pop_back();
 		return 0;
 	}
 
 	int pushr(){
-		if (dataStack.size() == 0) {
+		if (dataStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		returnStack.push_back(dataStack.back());
-		dataStack.pop_back();
+		returnStack->push_back(dataStack->back());
+		dataStack->pop_back();
 		return 0;
 	}
 
 	int drop(){
-		if (returnStack.size() == 0) {
+		if (returnStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		dataStack.pop_back();
+		dataStack->pop_back();
 		return 0;
 	}
 
 	int dup(){
 		
-		if (dataStack.size() == 0) {
+		if (dataStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		dataStack.push_back(dataStack.back());
+		dataStack->push_back(dataStack->back());
 		return 0;
 	}
 
 	int over(){
 
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		dataStack.push_back(dataStack[dataStack.size()-2]);
+		dataStack->push_back(*(dataStack->end()-2));
 		return 0;
 	}
 
 	int swap(){
 
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a);
-		dataStack.push_back(b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a);
+		dataStack->push_back(b);
 		return 0;
 
 	}
 
 	int sub()
 	{
-		if (dataStack.size() < 2)
+		if (dataStack->size() < 2)
 		{
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a - b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a - b);
 		return 0;
 	}
 
 	int add(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a + b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a + b);
 		return 0;
 	}
 
 	int mul(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a * b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a * b);
 		return 0;
 	}
 
 	int div(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a / b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a / b);
 		return 0;
 	}
 
 	int mod(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a % b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a % b);
 		return 0;
 	}
 
 	int bit_and(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a & b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a & b);
 		return 0;
 	}
 
 	int bit_or(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a | b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a | b);
 		return 0;
 	}
 
 	int bit_xor(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		dataStack.pop_back();
-		int b = dataStack.back();
-		dataStack.pop_back();
-		dataStack.push_back(a ^ b);
+		int a = dataStack->back();
+		dataStack->pop_back();
+		int b = dataStack->back();
+		dataStack->pop_back();
+		dataStack->push_back(a ^ b);
 		return 0;
 	}
 
 	int cmp(){
-		if (dataStack.size() < 2) {
+		if (dataStack->size() < 2) {
 			throw runtime_error("Stack underflow error");
 		}
-		int a = dataStack.back();
-		int b = dataStack[dataStack.size()-2];
+		int a = dataStack->back();
+		int b = *(dataStack->end()-2);
 		if( a > b ){
 			gt=1;
 			eq=0;
@@ -208,48 +208,48 @@ class ALU{
 
 	int gt_jump( int operand ){
 		if( gt ){
-			pc = operand;
+			*pc = operand;
 		}
 		return 0;
 	}
 
 	int eq_jump( int operand ){
 		if( eq ){
-			pc = operand;
+			*pc = operand;
 		}
 		return 0;
 	}
 
 	int jump( int operand ){
-		pc = operand;
+		*pc = operand;
 		return 0;
 	}
 
 	int ret(){
-		if (returnStack.size() == 0) {
+		if (returnStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		pc = returnStack.back();
-		returnStack.pop_back();
+		*pc = returnStack->back();
+		returnStack->pop_back();
 		return 0;
 	}
 
 	int call( int operand ){
-		returnStack.push_back(pc);
-		pc = operand;
+		returnStack->push_back(*pc + 1);
+		*pc = operand;
 		return 0;
 	}
 
 	int halt(){
-		pc = -1;
+		*pc = -1;
 		return 0;
 	}
 
 	int print(){
-		if (dataStack.size() == 0) {
+		if (dataStack->size() == 0) {
 			throw runtime_error("Stack underflow error");
 		}
-		cout << dataStack.back() << endl;
+		cout << dataStack->back() << endl;
 		return 0;
 	}
 
